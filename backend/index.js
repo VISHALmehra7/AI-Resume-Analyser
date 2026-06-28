bae39f2af03e73db3e49018e12fee30d4a9bf205
VISHALmehra7 <vishalmhr7@gmail.com>
2026-06-28 21:02:46 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ import path from 'path'
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","https://ai-resume-analyser-l0cm.onrender.com"], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
